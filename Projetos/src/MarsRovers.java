11e51a4c8034d19650ffba62a5a5bdeccdb8a5ac
ArturAquino <uchihaartur@yahoo.com.br>
2017-01-20 15:34:25 -0300

    Update MarsRovers.java

--- Projetos/src/MarsRovers.java
+++ Projetos/src/MarsRovers.java
@@ -7,22 +7,39 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+import static jdk.nashorn.internal.runtime.JSType.isNumber;
+
 public class MarsRovers{
 	
 	public static void main(String[] args){
-
-		int qtdRobos = 0;
 
 		String[] tamPlato = null;
 		String[] posRobo = null;
 		String comando = null;
 
+		System.out.println("Informe a quantidade de Robos: ");
+
+		String qtdRobos = entradaDados();
+		int robos = 0;
+
+		if(isNumber(qtdRobos)){
+			robos = Integer.parseInt(qtdRobos);
+		}else {
+			System.out.println("Informe SOMENTE numeros!");
+			return;
+		}
+
+		System.out.println("Informe o tamanho do Plato: ");
 		tamPlato = entradaDados().split(" ");
 
 		Plato plato = new Plato(Integer.parseInt(tamPlato[0]), Integer.parseInt(tamPlato[1]));
 		
-		for(int i = 0; i < 2; i++){
+		for(int i = 0; i < robos; i++){
+
+			System.out.println("Informe a posicao inicial do robo "+(i+1)+": ");
 			posRobo = entradaDados().toUpperCase().split(" ");
+
+			System.out.println("Passe o(s) comando(s): ");
 			comando = entradaDados();
 			
 			Coordenada coord = new Coordenada(Integer.parseInt(posRobo[0]), Integer.parseInt(posRobo[1]));
@@ -30,7 +47,7 @@ public class MarsRovers{
 			
 			expRobo.executarCmd(comando);
 			
-			System.out.println("Posicao do Robo "+i+": "+expRobo.localizacao());
+			System.out.println("Posicao do Robo "+(i+1)+": "+expRobo.localizacao());
 			
 			
 		}
@@ -56,7 +73,4 @@ public class MarsRovers{
 			return null;
 		}
 	}
-	
-	
-	
 }
